7ddb5fc2496d52c5ecc1a01ad13744d28a7f5ffa
jhoermann <jochen.hoermann@gmail.com>
2018-10-25 18:37:56 +0200

    Use graphics magick instead of convert

--- PDF2Image.js
+++ PDF2Image.js
@@ -1,28 +1,19 @@
-const fs = require('mz/fs');
-const path = require('path');
-const _ = require('lodash');
-const WithTmpDir = require('with-tmp-dir-promise').WithTempDir;
-const exec = require('mz/child_process').exec;
+const gm = require('gm');
 const {requireNativeExecutableSync} = require('require-native-executable');
 
-requireNativeExecutableSync('convert');
+requireNativeExecutableSync('gm');
 
 async function pdfToImage (pdfBuf, imgtype = 'jpg', dpi = 200) {
-    return WithTmpDir(async (tmpdir) => {
-        const srcpath = path.join(tmpdir, 'in.pdf');
-        const outpath = path.join(tmpdir, `out-%04d.${imgtype}`);
-        // Write input files
-        await fs.writeFile(srcpath, pdfBuf);
-        // Convert
-        const cmd = `convert -format ${imgtype} -density ${dpi} ${srcpath} -quality 95 ${outpath}`;
-        await exec(cmd);
-        // Read all the files
-        const files = await fs.readdir(tmpdir);
-        const imgs = files.filter(file => _.endsWith(file, `.${imgtype}`)).sort();
-        const imgPaths = imgs.map(img => path.join(tmpdir, img));
-        const imgPromises = imgPaths.map(img => fs.readFile(img));
-        return Promise.all(imgPromises);
-    }, { unsafeCleanup: true, prefix: 'pdftoolz-pdf2img-' });
+   return new Promise((resolve, reject) => {
+      gm(pdfBuf)
+         .density(dpi,dpi)
+         .quality(95)
+         .toBuffer(imgtype, function (err, buffer) {
+            if (err) reject('Error generating pdf preview pic: ' + err);
+            let img = Buffer.from(buffer, 'binary');
+            resolve(img);
+         });
+   });
 }
 
 // test().then(console.log).catch(console.error);
